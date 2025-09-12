9df9d09301ed7060725184ad2715af0b8490f0af
wisdomtochi <agbasionyewisdom@gmail.com>
2025-09-12 01:21:13 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Zippy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
